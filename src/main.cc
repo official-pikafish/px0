01bf77d839f227fc0cd143c3a255ea7fa7450157
Alexander Lyashuk <crem@google.com>
2025-04-06 19:03:13 +0200

    Remove EngineLoop class
    * Move Preload off engine_loop into engines
    
    * Make Engine params also populateable.
    
    * Moved UCI loop outside of the UCI class.
    
    * Compiles.
    
    * LogFile handling.
    
    * bugfix

--- src/main.cc
+++ src/main.cc
@@ -108,12 +108,7 @@ int main(int argc, const char** argv) {
           used_new_search = true;
           SearchFactory* factory =
               SearchManager::Get()->GetFactoryByName(search_name);
-          factory->PopulateParams(options_parser.get());
-          StdoutUciResponder uci_responder;
-          EngineLoop loop(&uci_responder, options_parser.get(),
-                          std::make_unique<Engine>(
-                              *factory, options_parser->GetOptionsDict()));
-          loop.RunLoop();
+          RunEngine(factory);
         }
       }
 
@@ -121,12 +116,7 @@ int main(int argc, const char** argv) {
         // Consuming optional "uci" mode.
         CommandLine::ConsumeCommand("uci");
         // Ordinary UCI engine.
-        EngineClassic::PopulateOptions(options_parser.get());
-        StdoutUciResponder uci_responder;
-        EngineLoop loop(
-            &uci_responder, options_parser.get(),
-            std::make_unique<EngineClassic>(options_parser->GetOptionsDict()));
-        loop.RunLoop();
+        RunEngineClassic();
       }
     }
   } catch (std::exception& e) {
